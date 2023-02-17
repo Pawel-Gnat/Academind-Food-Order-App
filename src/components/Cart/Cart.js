ab5fc84ba173e7af553554f5eac1796376a8ca88
Paweł <pawel_gnat@o2.pl>
2023-02-17 19:03:31 +0100

    chore: sending form to firebase

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,6 +7,8 @@ import Checkout from './Checkout'
 
 const Cart = props => {
 	const [isCheckout, setIsCheckout] = useState(false)
+	const [isSubmitting, setIsSubmitting] = useState(false)
+	const [didSubmit, setDidSubmit] = useState(false)
 	const cartCtx = useContext(CartContext)
 
 	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`
@@ -22,6 +24,22 @@ const Cart = props => {
 
 	const orderHandler = () => {
 		setIsCheckout(true)
+	}
+
+	const submitOrderHandler = async userData => {
+		setIsSubmitting(true)
+
+		await fetch('https://food-app-a1ef1-default-rtdb.europe-west1.firebasedatabase.app/orders.json', {
+			method: 'POST',
+			body: JSON.stringify({
+				user: userData,
+				orderedItems: cartCtx.items,
+			}),
+		})
+
+		setIsSubmitting(false)
+		setDidSubmit(true)
+		cartCtx.clearCart()
 	}
 
 	const cartItems = (
@@ -56,15 +74,32 @@ const Cart = props => {
 		</div>
 	)
 
-	return (
-		<Modal onClose={props.onClose}>
+	const cartModalContent = (
+		<>
 			{cartItems}
 			<div className={classes.total}>
 				<span>Total Amount</span>
 				<span>{totalAmount}</span>
 			</div>
-			{isCheckout && <Checkout onCancel={props.onClose} />}
+			{isCheckout && (
+				<Checkout
+					onConfirm={submitOrderHandler}
+					onCancel={props.onClose}
+				/>
+			)}
 			{!isCheckout && modalActions}
+		</>
+	)
+
+	const isSubmittingModalContent = <p>Sending order data...</p>
+
+	const didSubmitModalContent = <p>Successfully sent the order!</p>
+
+	return (
+		<Modal onClose={props.onClose}>
+			{!isSubmitting && !didSubmit && cartModalContent}
+			{isSubmitting && isSubmittingModalContent}
+			{!isSubmitting && didSubmit && didSubmitModalContent}
 		</Modal>
 	)
 }
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,4 +1,5 @@
 import { useReducer } from 'react'
+
 import CartContext from './cart-context'
 
 const defaultCartState = {
@@ -11,7 +12,6 @@ const cartReducer = (state, action) => {
 		const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
 
 		const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id)
-
 		const existingCartItem = state.items[existingCartItemIndex]
 		let updatedItems
 
@@ -31,17 +31,15 @@ const cartReducer = (state, action) => {
 			totalAmount: updatedTotalAmount,
 		}
 	}
-
 	if (action.type === 'REMOVE') {
 		const existingCartItemIndex = state.items.findIndex(item => item.id === action.id)
-		const existingCartItem = state.items[existingCartItemIndex]
-		const updatedTotalAmount = state.totalAmount - existingCartItem.price
+		const existingItem = state.items[existingCartItemIndex]
+		const updatedTotalAmount = state.totalAmount - existingItem.price
 		let updatedItems
-
-		if (existingCartItem.amount === 1) {
+		if (existingItem.amount === 1) {
 			updatedItems = state.items.filter(item => item.id !== action.id)
 		} else {
-			const updatedItem = { ...existingCartItem, amount: existingCartItem.amount - 1 }
+			const updatedItem = { ...existingItem, amount: existingItem.amount - 1 }
 			updatedItems = [...state.items]
 			updatedItems[existingCartItemIndex] = updatedItem
 		}
@@ -52,6 +50,10 @@ const cartReducer = (state, action) => {
 		}
 	}
 
+	if (action.type === 'CLEAR') {
+		return defaultCartState
+	}
+
 	return defaultCartState
 }
 
@@ -59,16 +61,15 @@ const CartProvider = props => {
 	const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
 
 	const addItemToCartHandler = item => {
-		dispatchCartAction({
-			type: 'ADD',
-			item: item,
-		})
+		dispatchCartAction({ type: 'ADD', item: item })
 	}
+
 	const removeItemFromCartHandler = id => {
-		dispatchCartAction({
-			type: 'REMOVE',
-			id: id,
-		})
+		dispatchCartAction({ type: 'REMOVE', id: id })
+	}
+
+	const clearCartHandler = () => {
+		dispatchCartAction({ type: 'CLEAR' })
 	}
 
 	const cartContext = {
@@ -76,6 +77,7 @@ const CartProvider = props => {
 		totalAmount: cartState.totalAmount,
 		addItem: addItemToCartHandler,
 		removeItem: removeItemFromCartHandler,
+		clearCart: clearCartHandler,
 	}
 
 	return <CartContext.Provider value={cartContext}>{props.children}</CartContext.Provider>
